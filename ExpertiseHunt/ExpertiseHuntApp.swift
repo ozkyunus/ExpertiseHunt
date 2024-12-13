e0b8bba13a31539ab2fe912f9024289a242b2272
Yunus Emre Özkaya <130933679+ozkyunus@users.noreply.github.com>
2024-12-13 17:37:09 +0300

    Initial Commit

---
+++ ExpertiseHunt/ExpertiseHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpertiseHuntApp.swift
+//  ExpertiseHunt
+//
+//  Created by Yunus Emre Özkaya on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpertiseHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
